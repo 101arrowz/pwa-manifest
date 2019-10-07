9c0c19b27829bb937ccf0b6ed135bb65b533232c
101arrowz <arjunbarrett@gmail.com>
2019-10-06 22:49:46 -0700

    Windows support

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { writeFileSync } = require('fs');
-const { join, relative } = require('path');
+const { join, relative, sep } = require('path');
 module.exports = bundler => {
   const { publicURL, outDir } = bundler.options;
   if (!publicURL.endsWith('/'))
     publicURL += '/';
   const getOptions = (entryAsset) => (typeof entryAsset.getPackage === 'function' ? entryAsset.getPackage() : Promise.resolve(entryAsset.package)).then(pkg => pkg['pwaManifest'] || pkg['pwa-manifest'] || {});
   const addBundledFiles = (bundle, arr) => {
-    arr.push(publicURL+relative(outDir, bundle.name));
+    arr.push(publicURL+relative(outDir, bundle.name).split(sep).join('/'));
     for (let childBundle of (bundle.childBundles || [])) addBundledFiles(childBundle, arr);
   }
   bundler.on('bundled', bundle => 
