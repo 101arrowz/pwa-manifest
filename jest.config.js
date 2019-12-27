39e6a37d29a905b45c4ca61e8b2c6d82abdd1523
101arrowz <arjunbarrett@gmail.com>
2019-12-27 01:29:33 -0800

    ????

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
     '<rootDir>/packages/*'
   ],
   collectCoverage: true,
-  coverageDirectory: 'coverageReport'
+  coverageDirectory: 'coverageReport',
+  moduleNameMapper: { // ts-jest prefers directly pulling in the .ts source file
+    '@pwa-manifest/core': '<rootDir>/packages/core/lib/index.js',
+    'parcel-plugin-pwa-manifest': '<rootDir>/packages/parcel-plugin-pwa-manifest/lib/index.js',
+    'webpack-plugin-pwa-manifest': '<rootDir>/packages/webpack-plugin-pwa-manifest/lib/index.js'
+  }
 }
